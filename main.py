9b8f3964fb6c5c77ad4998affef183962185c98c
MatthiasSagerer <msagerer1@gmail.com>
2021-02-04 16:24:45 +0100

    sodukus.py: adding easy1, medium1 & hard1 solutions.py: adding easy1, medium1 & hard1

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ while playing:
     # TODO: take difficulty as input and set difficulty variable (1, 2, 3 or 4)
     difficulty = 0
     while difficulty != 0:
+        pass
         # TODO: main solving screen with 9 x 9 grid, time, pause & play, digits 1 to 9 as inputs, undo & redo, hint and show solution button.
 
 
