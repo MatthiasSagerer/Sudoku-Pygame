e19c2bd860c15a1d28dbe9748fc6d11767c772af
MatthiasSagerer <msagerer1@gmail.com>
2021-02-04 15:44:31 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+playing = True
+
+while playing:
+    # TODO: Welcome Screen: Sudoku Headline, choosing disfficulty and displaying highscore (time) beneath the difficulties
+
+    # TODO: take difficulty as input and set difficulty variable (1, 2, 3 or 4)
+    difficulty = 0
+    while difficulty != 0:
+        # TODO: main solving screen with 9 x 9 grid, time, pause & play, digits 1 to 9 as inputs, undo & redo, hint and show solution button.
+
+
+
