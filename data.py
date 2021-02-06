c05465236903b2b1e95de3468a226f5f3588406e
MatthiasSagerer <msagerer1@gmail.com>
2021-02-06 16:35:54 +0100

    sudokus.py & solutions.py: HARD7 & EXPERT7

--- data.py
+++ data.py
@@ -2,17 +2,23 @@ import sudokus
 import solutions
 
 sudokus = {
-    'easy': [sudokus.EASY1, sudokus.EASY2, sudokus.EASY3, sudokus.EASY4, sudokus.EASY5, sudokus.EASY6],
-    'medium': [sudokus.MEDIUM1, sudokus.MEDIUM2, sudokus.MEDIUM3, sudokus.MEDIUM4, sudokus.MEDIUM5, sudokus.MEDIUM6],
-    'hard': [sudokus.HARD1, sudokus.HARD2, sudokus.HARD3, sudokus.HARD4, sudokus.HARD5, sudokus.HARD6],
-    'expert': [sudokus.EXPERT1, sudokus.EXPERT2, sudokus.EXPERT3, sudokus.EXPERT4, sudokus.EXPERT5, sudokus.EXPERT6]
+    'easy': [sudokus.EASY1, sudokus.EASY2, sudokus.EASY3, sudokus.EASY4, sudokus.EASY5,
+             sudokus.EASY6],
+    'medium': [sudokus.MEDIUM1, sudokus.MEDIUM2, sudokus.MEDIUM3, sudokus.MEDIUM4,
+               sudokus.MEDIUM5, sudokus.MEDIUM6],
+    'hard': [sudokus.HARD1, sudokus.HARD2, sudokus.HARD3, sudokus.HARD4, sudokus.HARD5,
+             sudokus.HARD6, sudokus.HARD7],
+    'expert': [sudokus.EXPERT1, sudokus.EXPERT2, sudokus.EXPERT3, sudokus.EXPERT4,
+               sudokus.EXPERT5, sudokus.EXPERT6, sudokus.EXPERT7]
 }
 
 solutions = {
-    'easy': [solutions.EASY1, solutions.EASY2, solutions.EASY3, solutions.EASY4, solutions.EASY5, solutions.EASY6],
-    'medium': [solutions.MEDIUM1, solutions.MEDIUM2, solutions.MEDIUM3, solutions.MEDIUM4, solutions.MEDIUM5,
-               solutions.MEDIUM6],
-    'hard': [solutions.HARD1, solutions.HARD2, solutions.HARD3, solutions.HARD4, solutions.HARD5, solutions.HARD6],
-    'expert': [solutions.EXPERT1, solutions.EXPERT2, solutions.EXPERT3, solutions.EXPERT4, solutions.EXPERT5,
-               solutions.EXPERT6]
+    'easy': [solutions.EASY1, solutions.EASY2, solutions.EASY3, solutions.EASY4, solutions.EASY5,
+             solutions.EASY6],
+    'medium': [solutions.MEDIUM1, solutions.MEDIUM2, solutions.MEDIUM3, solutions.MEDIUM4,
+               solutions.MEDIUM5, solutions.MEDIUM6],
+    'hard': [solutions.HARD1, solutions.HARD2, solutions.HARD3, solutions.HARD4, solutions.HARD5,
+             solutions.HARD6, solutions.HARD7],
+    'expert': [solutions.EXPERT1, solutions.EXPERT2, solutions.EXPERT3, solutions.EXPERT4,
+               solutions.EXPERT5, solutions.EXPERT6, solutions.EXPERT7]
 }
