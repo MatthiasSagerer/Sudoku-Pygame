73e240a2fc44685c3cce55977689902d6387bd5f
MatthiasSagerer <msagerer1@gmail.com>
2021-02-07 19:36:28 +0100

    sudokus.py & solutions.py: VERY_HARD1, VERY_HARD2, VERY_HARD3, VERY_HARD4, VERY_HARD5, VERY_HARD6, VERY_HARD7

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ sudokus = {
                sudokus.MEDIUM5, sudokus.MEDIUM6, sudokus.MEDIUM7],
     'hard': [sudokus.HARD1, sudokus.HARD2, sudokus.HARD3, sudokus.HARD4, sudokus.HARD5,
              sudokus.HARD6, sudokus.HARD7],
-    'very hard': [sudokus.VERYHARD1, sudokus.VERYHARD2, sudokus.VERYHARD3, sudokus.VERYHARD4,
-               sudokus.VERYHARD5, sudokus.VERYHARD6, sudokus.VERYHARD7]
+    'very hard': [sudokus.VERY_HARD1, sudokus.VERY_HARD2, sudokus.VERY_HARD3, sudokus.VERY_HARD4,
+               sudokus.VERY_HARD5, sudokus.VERY_HARD6, sudokus.VERY_HARD7]
 }
 
 solutions = {
@@ -19,6 +19,6 @@ solutions = {
                solutions.MEDIUM5, solutions.MEDIUM6, solutions.MEDIUM7],
     'hard': [solutions.HARD1, solutions.HARD2, solutions.HARD3, solutions.HARD4, solutions.HARD5,
              solutions.HARD6, solutions.HARD7],
-    'very hard': [solutions.VERYHARD1, solutions.VERYHARD2, solutions.VERYHARD3, solutions.VERYHARD4,
-               solutions.VERYHARD5, solutions.VERYHARD6, solutions.VERYHARD7]
+    'very hard': [solutions.VERY_HARD1, solutions.VERY_HARD2, solutions.VERY_HARD3, solutions.VERY_HARD4,
+               solutions.VERY_HARD5, solutions.VERY_HARD6, solutions.VERY_HARD7]
 }
